25f6486eab8bce0bb311f78bf57c9d39eb6de1fa
h552hgg <h552hgg@gmail.com>
2021-01-24 20:51:37 -0600

    level 1 done. Need comments

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,80 +4,42 @@ var tableData = data;
 // Select the table body
 var tbody = d3.select("tbody");
 
-// // // Verify reading of the data
-// // console.log(tableData)
-
-// //Loop through the data
-// tableData.forEach(function(sighting){
-//     // //View the return
-//     // console.log(sighting)
-//     //append the rows
-//     var row = tbody.append("tr");
-//     Object.entries(sighting).forEach(function([key,value]){
-//         // //View the return
-//         // console.log(key,value);
-
-//         //add data to row
-//         var cell = row.append("td");
-//     });
-
-// });
-
-
+function buildTable(tableData){
 // Use the arrow function to loop through the data 
-tableData.forEach((sighting) => {
-    //add to rows
-    var row = tbody.append("tr");
-    //Inner loop pull out keys and value
-    Object.entries(sighting).forEach(([key, value]) => {
-        //add cells/ table data to row
-        var cell = row.append("td");
-        //Use the value for the table data
-        cell.text(value);
+    tbody.html("");
+    tableData.forEach((sighting) => {
+        //add to rows
+        var row = tbody.append("tr");
+        //Inner loop pull out keys and value
+        Object.entries(sighting).forEach(([key, value]) => {
+            //add cells/ table data to row
+            var cell = row.append("td");
+            //Use the value for the table data
+            cell.text(value);
+        });
     });
-});
-
-///Button/listner section
-
-
-//Select button
-var button = d3.select("#button");
-
-//Select form
-var form = d3.select("#form");
-
-button.on("click", runEnter);
-form.on("submit",runEnter);
-
+}
 
 function runEnter() {
 
     
-    d3.event.preventDefault();
+    // d3.event.preventDefault();
     
 
-    var inputElement = d3.select("#datetime");
-  
-    var inputValue = inputElement.property("value");
-  
-    console.log(inputValue);
-    console.log(tableData);
-  
-    var filteredData = tableData.filter(x => x.datetime === inputValue);
-  
-    console.log(filteredData);
+    const inputElement = d3.select("#datetime").property("value");
+    let filteredTable = tableData;
 
 
-      
-    // var list = d3.select(".filters");
-
-  
-    // list.html("");
+    if (inputElement) {
+        filteredTable = filteredTable.filter(row => row.datetime === inputElement);
+    }
 
 
-    // list.append("li").text(`Mean: ${filteredData}`);
-  
+    buildTable(filteredTable);
 
-  
-  
-};
+}
+
+d3.selectAll("#filter-btn").on("click", runEnter);
+
+buildTable(tableData);
+
